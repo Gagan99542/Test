db1a8899b65fb65f6e8c9a38b1008631f97e71ed
Gagan99542 <gagansai777@gmail.com>
2021-07-01 13:21:16 +0530

    Updates test file

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
+test = input('Are you testing ? \n')
+if test == "yes":
+    print('U are most welcome to do so....')
+else:
+    print('Sorry....no access for u to change....')
 print('This was a sample file to upload in git')
